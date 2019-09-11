ec573a8b1765944cec779ffb3b59614036e789c4
Teison De oleo Montero <teisonmontero@MacBook-Air-de-Teison.local>
2019-09-11 18:11:39 -0400

    inicio

---
+++ js/code.js
@@ -0,0 +1,11 @@
+// Limpiar el forms
+
+let clear = document.getElementById("limpiar");
+clear.addEventListener("click", limpiarForm);
+
+function limpiarForm(){
+  document.getElementById("myForm").reset();
+}
+
+// validar formulario
+
